f8bff2e01458e4f8914346863837c94c628b891f
TrustworthyBlake <andreasblakli@gmail.com>
2020-11-04 16:54:49 +0100

    Added bullet projectile

---
+++ Source/artificialLife/bullet.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "bullet.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+Abullet::Abullet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bulletMesh = CreateDefaultSubobject<UStaticMeshComponent>("bulletMesh");
+}
+
+// Called when the game starts or when spawned
+void Abullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Abullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
